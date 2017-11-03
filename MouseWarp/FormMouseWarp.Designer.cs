4cbcffdf97e9af96b1967e5d6194fab0a2009b20
rkr <rkr@remmert.de>
2017-11-03 13:10:18 +0100

    Initial Checkin

---
+++ MouseWarp/FormMouseWarp.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace MouseWarp
+{
+    partial class FormMouseWarp
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // TrayIcon
+            // 
+            this.TrayIcon.Text = "notifyIcon1";
+            this.TrayIcon.Visible = true;
+            this.TrayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.TrayIcon_MouseDoubleClick);
+            // 
+            // FormMouseWarp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(121, 1);
+            this.Name = "FormMouseWarp";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMouseWarp_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon TrayIcon;
+    }
+}
+
---
+++ MouseWarp/FormMouseWarp.cs
@@ -0,0 +1,59 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MouseWarp
+{
+    public partial class FormMouseWarp : Form
+    {
+        private readonly IKeyboardMouseEvents _hook;
+
+        public FormMouseWarp()
+        {
+            InitializeComponent();
+
+            var resourceManager = new ResourceManager(typeof(FormMouseWarp));
+            var bmp = (Bitmap)resourceManager.GetObject("Icon");
+            var handle = bmp.GetHicon();
+
+            TrayIcon.Icon = Icon.FromHandle(handle);
+
+            DestroyIcon(handle);
+
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+
+            // https://github.com/gmamaladze/globalmousekeyhook
+            _hook = Hook.GlobalEvents();
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        extern private static bool DestroyIcon(IntPtr handle);
+
+        private void OnMouseMoveExt(object sender, MouseEventExtArgs e)
+        {
+            var screen = Screen.AllScreens.FirstOrDefault(s => e.X >= s.Bounds.Left && e.X <= s.Bounds.Right);
+            if (screen == null)
+                return;
+
+            if (e.Y <= screen.Bounds.Top)
+                Cursor.Position = new Point(e.X, screen.Bounds.Top - 1);
+            else if (e.Y >= screen.Bounds.Bottom)
+                Cursor.Position = new Point(e.X, screen.Bounds.Bottom - 1);
+        }
+
+        private void FormMouseWarp_Load(object sender, EventArgs e)
+        {
+            _hook.MouseMoveExt += OnMouseMoveExt;
+        }
+
+        private void TrayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
